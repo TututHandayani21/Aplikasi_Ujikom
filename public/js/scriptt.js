89f86c05d6ed05d1eebe32163e76a67a873b8fb9
TututHandayani21 <2223618.tutut@smkn-2sbg.sch.id>
2025-02-15 13:01:06 +0700

    Menambahkan Foto profile

--- public/js/scriptt.js
+++ public/js/scriptt.js
@@ -10,3 +10,12 @@ allSideMenu.forEach(item=> {
           li.classList.add('active');
           })
 });
+
+
+// Toggle Sidebar
+const menuBar = document.querySelector ('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+          sidebar.classList.toggle('hide');
+})
