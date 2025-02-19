505fcbdc88d6fbb30573919b64248698707ef441
TututHandayani21 <2223618.tutut@smkn-2sbg.sch.id>
2025-02-19 13:55:08 +0700

    Menambahkan komentar di modal

--- resources/views/Pages/profile.blade.php
+++ resources/views/Pages/profile.blade.php
@@ -1,36 +1,62 @@
+<!-- Struktur dasar dokumen HTML -->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <!-- Metadata dan pengaturan tampilan responsif -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Menghubungkan ikon dari Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+    
     <title>My Profile</title>
+    
+    <!-- Menghubungkan stylesheet eksternal -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 </head>
 
-<body>                                                                                                                                                  
+<body>
     <section>
+        <!-- Container utama yang membungkus konten utama -->
         <div class="main-container">
+            
+            <!-- Bagian untuk menampilkan gambar profil -->
             <div class="image " data-aos="zoom-in-right" data-aos-duration="2500" style="width: 40%; height:40%">
                 <img src="{{ asset('foto/Profile.jpg') }}">
             </div>
+            
+            <!-- Bagian konten teks -->
             <div class="content">
-                <h1 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="800">Hey I'm <span>Tutut
-                        Handayani</span></h1>
-                <h2 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="800">I'm a <span>This is the story of
-                        the lasht Child</span></h2>
-                        <h3 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="800"><span>Indramayu,20 Juni 2006</span></h3>
+                <h1 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="800">
+                    Hey I'm <span>Tutut Handayani</span>
+                </h1>
+                
+                <h2 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="800">
+                    I'm a <span>This is the story of the last Child</span>
+                </h2>
+                
+                <h3 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="800">
+                    <span>Indramayu, 20 Juni 2006</span>
+                </h3>
+                
                 <div>
-                    <h3 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1000">Hallo aku adalah anak terakhir dari 4 bersaudara, Harapan terakhir keluarga meskipun itu sulit untuk aku, tapi aku selalu berusaha yang terbaik untuk mengangkat darjat orang tua.</h3>
+                    <h3 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1000">
+                        Hallo aku adalah anak terakhir dari 4 bersaudara, Harapan terakhir keluarga meskipun itu sulit untuk aku,
+                        tapi aku selalu berusaha yang terbaik untuk mengangkat derajat orang tua.
+                    </h3>
                 </div>
+                
+                <!-- Ikon media sosial -->
                 <div class="social-links" data-aos="flip-down" data-aos-duration="1000" data-aos-delay="1200">
                     <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
                     <a href="#"><i class="fa-brands fa-instagram"></i></a>
                     <a href="#"><i class="fa-brands fa-github"></i></a>
                     <a href="#"><i class="fa-brands fa-telegram"></i></a>
                 </div>
+                
+                <!-- Tombol profil -->
                 <div class="btn" data-aos="zoom-out-left" data-aos-duration="1000" data-aos-delay="1300">
                     <button>My Profile</button>
                 </div>
@@ -38,27 +64,29 @@
         </div>
     </section>
 
-
-
+    <!-- Menghubungkan pustaka animasi AOS -->
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
         AOS.init({
             offset: 0
         });
     </script>
+
+    <!-- Skrip untuk navigasi dropdown -->
     <script>
         function hamburg() {
-            const navbar = document.querySelector(".dropdown")
-            navbar.style.transform = "translateY(0px)"
+            const navbar = document.querySelector(".dropdown");
+            navbar.style.transform = "translateY(0px)";
         }
 
         function cancel() {
-            const navbar = document.querySelector(".dropdown")
-            navbar.style.transform = "translateY(-500px)"
+            const navbar = document.querySelector(".dropdown");
+            navbar.style.transform = "translateY(-500px)";
         }
     </script>
 
     <style>
+        /* Reset margin dan padding */
         * {
             padding: 0;
             margin: 0;
@@ -66,10 +94,12 @@
             box-sizing: border-box;
         }
 
+        /* Menyembunyikan overflow horizontal */
         html {
             overflow-x: hidden;
         }
 
+        /* Styling untuk body dengan gradient background */
         body {
             width: 100%;
             height: 100vh;
@@ -77,126 +107,14 @@
             background: linear-gradient(to right, rgb(255, 255, 255), rgb(254, 215, 173));
         }
 
+        /* Navigasi */
         nav {
             width: 100%;
             height: 10vh;
             position: sticky;
         }
 
-        .nav-container {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-        }
-
-        .logo {
-            color: black;
-            font-size: 2rem;
-            font-weight: bolder;
-        }
-
-        .logo span {
-            color: rgb(109, 67, 0);
-            text-shadow: 0 0 10px rgb(109, 67, 0);
-        }
-
-        .hamburg,
-        .cancel {
-            cursor: pointer;
-            position: absolute;
-            right: 15px;
-            top: 10px;
-            color: black;
-            font-size: 2rem;
-            display: none;
-        }
-
-        .nav-container .links {
-            display: flex;
-            gap: 3rem;
-            align-items: center;
-        }
-
-        .nav-container .links a {
-            position: relative;
-            font-size: 1.2rem;
-            color: black;
-            text-decoration: none;
-            font-weight: 500;
-            transform: 0.3s linear;
-        }
-
-        .nav-container .links a::before {
-            position: absolute;
-            content: "";
-            bottom: -3px;
-            left: 0;
-            width: 0%;
-            height: 3px;
-            background-color: rgb(109, 67, 0);
-            transition: 0.2s linear;
-        }
-
-        .nav-container .links a:hover::before {
-            width: 100%;
-        }
-
-        .nav-container .contact-btn {
-            background-color: transparent;
-            padding: 5px 20px;
-            border-radius: 20px;
-            border: 2px solid rgb(109, 67, 0);
-            transition: 0.2s linear;
-        }
-
-        .nav-container .contact-btn a {
-            color: white;
-            transition: 0.3s linear;
-        }
-
-        .nav-container .contact-btn:hover,
-        .nav-container .contact-btn:hover a {
-            background-color: rgb(109, 67, 0);
-            color: white;
-        }
-
-        .nav-container .links a:hover {
-            color: rgb(109, 67, 0);
-        }
-
-        .dropdown {
-            z-index: 100;
-            position: absolute;
-            top: 0;
-            transform: translateY(-500px);
-            width: 100%;
-            height: auto;
-            backdrop-filter: brightness(40%) blur(3px);
-            box-shadow: 0 0 20px black;
-            transition: 0.2s linear;
-        }
-
-        .dropdown .links a {
-            color: white;
-            display: flex;
-            text-decoration: none;
-            justify-content: center;
-            padding: 15px 0;
-            align-items: center;
-            transition: 0.2s linear;
-        }
-
-        .dropdown .links a:hover {
-            background-color: rgb(109, 67, 0);
-        }
-
-        section {
-            width: 100%;
-            height: 90vh;
-        }
-
+        /* Container utama */
         .main-container {
             margin-top: 1rem;
             display: flex;
@@ -204,6 +122,7 @@
             align-items: center;
         }
 
+        /* Styling untuk gambar */
         .main-container .image {
             z-index: -1;
             width: 50%;
@@ -213,6 +132,7 @@
             width: 100%;
         }
 
+        /* Konten teks */
         .main-container .content {
             color: black;
             width: 40%;
@@ -228,62 +148,7 @@
             text-shadow: 0 0 10px rgb(109, 67, 0);
         }
 
-        .content .typewriter {
-            font-weight: 600;
-            font-size: clamp(1rem, 1rem + 5vw, 2rem);
-        }
-
-        .content .typewriter span {
-            color: rgb(109, 67, 0);
-            font-size: 2.5rem;
-            text-shadow: 0 0 5px rgb(109, 67, 0);
-            position: relative;
-            transition: 0.3s linear;
-        }
-
-        .typewriter span::before {
-            content: "Developer";
-            animation: words 15s infinite;
-        }
-
-        .typewriter span::after {
-            content: "";
-            position: absolute;
-            width: calc(100% + 8px);
-            height: 100%;
-            border-left: 3px solid black;
-            animation: cursor 0.6s linear infinite;
-        }
-
-        @keyframes cursor {
-            to {
-                border-left: 3px solid #b74b4b;
-            }
-        }
-
-        @keyframes words {
-
-            0%,
-            33% {
-                content: "Developer";
-            }
-
-            34%,
-            66% {
-                content: "Designer";
-            }
-
-            67%,
-            100% {
-                content: "Youtuber";
-            }
-        }
-
-        .content p {
-            font-size: clamp(0.4rem, 0.2rem + 9vw, 1rem);
-            margin: 10px 0;
-        }
-
+        /* Media sosial */
         .social-links i {
             display: inline-flex;
             justify-content: center;
@@ -306,6 +171,7 @@
             filter: drop-shadow(0 0 10px rgb(109, 67, 0));
         }
 
+        /* Tombol profil */
         .content button {
             width: 40%;
             height: 6vh;
@@ -328,51 +194,6 @@
             font-weight: 700;
             box-shadow: 0 0 40px rgb(109, 67, 0);
         }
-
-        @media (max-width:884px) {
-            body {
-                overflow-y: visible;
-            }
-
-            nav .logo {
-                position: absolute;
-                top: 16px;
-                left: 15px;
-                font-size: 1.5rem;
-            }
-
-            .main-container {
-                display: flex;
-                flex-direction: column;
-            }
-
-            .nav-container .links {
-                display: none;
-            }
-
-            .hamburg,
-            .cancel {
-                display: block;
-            }
-
-            .main-container .content {
-                width: 80%;
-            }
-
-            .social-links i {
-                width: 2.5rem;
-                height: 2.5rem;
-                font-size: 1.5rem;
-            }
-
-            .cancel {
-                color: white;
-            }
-
-            .main-container .image {
-                width: 80%;
-                margin-bottom: 0px;
-            }
     </style>
 </body>
 
--- resources/views/Partials/modal.blade.php
+++ resources/views/Partials/modal.blade.php
@@ -1,12 +1,16 @@
+<!-- Modal untuk menambahkan List -->
 <div class="modal fade" id="addListModal" tabindex="-1" aria-labelledby="addListModalLabel" aria-hidden="true">
     <div class="modal-dialog">
+        <!-- Form untuk menyimpan List baru -->
         <form action="{{ route('lists.store') }}" method="POST" class="modal-content">
-            @method('POST')
-            @csrf
+            @method('POST') <!-- Metode HTTP POST untuk menyimpan data -->
+            @csrf <!-- Token keamanan CSRF untuk mencegah serangan CSRF -->
+            
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="addListModalLabel">Tambah List</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            
             <div class="modal-body">
                 <div class="mb-3">
                     <label for="name" class="form-label">Nama</label>
@@ -14,6 +18,7 @@
                         placeholder="Masukkan nama list">
                 </div>
             </div>
+            
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
                 <button type="submit" class="btn btn-primary">Tambah</button>
@@ -22,27 +27,34 @@
     </div>
 </div>
 
+<!-- Modal untuk menambahkan Task -->
 <div class="modal fade" id="addTaskModal" tabindex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
     <div class="modal-dialog">
+        <!-- Form untuk menyimpan Task baru -->
         <form action="{{ route('tasks.store') }}" method="POST" class="modal-content">
-            @method('POST')
-            @csrf
+            @method('POST') <!-- Metode HTTP POST untuk menyimpan data -->
+            @csrf <!-- Token keamanan CSRF untuk mencegah serangan CSRF -->
+            
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="addTaskModalLabel">Tambah Task</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            
             <div class="modal-body">
-                <input type="text" id="taskListId" name="list_id" hidden>
+                <input type="text" id="taskListId" name="list_id" hidden> <!-- ID List untuk menghubungkan Task ke List tertentu -->
+                
                 <div class="mb-3">
                     <label for="name" class="form-label">Nama</label>
                     <input type="text" class="form-control" id="name" name="name"
-                        placeholder="Masukkan nama list">
+                        placeholder="Masukkan nama task">
                 </div>
+                
                 <div class="mb-3">
                     <label for="description" class="form-label">Deskripsi</label>
                     <input type="text" class="form-control" id="description" name="description"
-                        placeholder="Masukkan nama list">
+                        placeholder="Masukkan deskripsi task">
                 </div>
+                
                 <div class="mb-3">
                     <label for="priority" class="form-label">Priority</label>
                     <select class="form-select form-select" aria-label="small select example" id="priority"
@@ -53,6 +65,7 @@
                     </select>
                 </div>
             </div>
+            
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
                 <button type="submit" class="btn btn-primary">Tambah</button>
