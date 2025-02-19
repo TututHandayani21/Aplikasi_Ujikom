b01a361b6c8a8c8237b5ce08a07b3e1ef0d019eb
TututHandayani21 <2223618.tutut@smkn-2sbg.sch.id>
2025-02-19 11:36:45 +0700

    Menambahkan komentar di TaskSeeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,29 +4,28 @@ namespace Database\Seeders;
 
 use App\Models\Task;
 use App\Models\TaskList;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TaskSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Menjalankan seeder untuk memasukkan data tugas ke dalam database.
      */
     public function run(): void
     {
-        //Detail dari judul
+        // Array daftar tugas yang akan dimasukkan ke dalam database
         $tasks = [
             [
                 'name' => 'Belajar Laravel',
                 'description' => 'Belajar Laravel di santri koding',
-                'is_completed' => false,
-                'priority' => 'medium',
-                'list_id' => TaskList::where('name', 'Belajar')->first()->id,
+                'is_completed' => false, // Status tugas belum selesai
+                'priority' => 'medium', // Prioritas tugas
+                'list_id' => TaskList::where('name', 'Belajar')->first()->id, // Mengambil ID dari TaskList dengan nama 'Belajar'
             ],
             [
                 'name' => 'Belajar React',
                 'description' => 'Belajar React di WPU',
-                'is_completed' => true,
+                'is_completed' => true, // Status tugas sudah selesai
                 'priority' => 'high',
                 'list_id' => TaskList::where('name', 'Belajar')->first()->id,
             ],
@@ -74,6 +73,7 @@ class TaskSeeder extends Seeder
             ],
         ];
 
+        // Memasukkan semua data tugas ke dalam tabel 'tasks' sekaligus
         Task::insert($tasks);
     }
 }
